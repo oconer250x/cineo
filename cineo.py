189008e4c8576af5872a7acdd0b314b3573019ac
oconer250x <mateo.bermejo@alumnos.udg.mx>
2024-08-28 17:58:32 -0600

    p5 layout

--- cineo.py
+++ cineo.py
@@ -5,7 +5,7 @@ cineoApp = Flask(__name__)
 
 @cineoApp.route('/')
 def home():
-    return render_template('index.html')
+    return render_template('home.html')
 
 if __name__ == "__main__":
     cineoApp.run(debug=True,port=3300)
