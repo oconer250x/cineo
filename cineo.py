8330ad5333ad3be51d4c5d9f9a20c7af4ac5f98e
oconer250x <mateo.bermejo5809@alumnos.udg.mx>
2024-09-03 18:56:36 -0600

    p6

--- cineo.py
+++ cineo.py
@@ -1,11 +1,18 @@
 from flask import Flask, render_template, url_for
 
+cineo = Flask(__name__)
 
-cineoApp = Flask(__name__)
-
-@cineoApp.route('/')
+@cineo.route('/')
 def home():
     return render_template('home.html')
 
+@cineo.route('/signin')
+def signin():
+    return render_template('signin.html')
+
+@cineo.route('/signup')
+def signup():
+    return render_template('signup.html')
+
 if __name__ == "__main__":
-    cineoApp.run(debug=True,port=3300)
+    cineo.run(debug=True,port=3300)
