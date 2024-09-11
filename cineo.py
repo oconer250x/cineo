57319f1b145ab499423ac9edbf8bce4715fe8573
oconer250x <mateo.bermejo5809@alumnos.udg.mx>
2024-09-10 18:00:41 -0600

    p9

--- cineo.py
+++ cineo.py
@@ -2,6 +2,7 @@ from flask import Flask, render_template, url_for, request, redirect
 from flask_mysqldb import MySQL
 from werkzeug.security import generate_password_hash
 from datetime import datetime
+from config import config
 
 
 cineo = Flask(__name__)
@@ -15,16 +16,20 @@ def home():
 def signin():
     return render_template('signin.html')
 
-@cineo.route('/signup')
+@cineo.route('/signup', methods=['POST','GET'])
 def signup():
     if request.method == 'POST':
         nombre = request.form['nombre']
         correo = request.form['correo']
         contraseña = request.form['password']
         claveCifrada = generate_password_hash(contraseña)
-        fechaReg = datetime.now() 
-
-    return render_template('signup.html')
+        fechaReg = datetime.now()
+        regUsuario = db.connection.cursor()
+        regUsuario.execute("INSERT INTO usuario (nombre, correo, clave,fechareg) VALUES (%s, %s, %s, %s),", (nombre,correo,claveCifrada,fechaReg)) 
+        db.connection.commit()
+        return render_template(home.html)
+    else:
+        return render_template('signup.html')
 
 if __name__ == "__main__":
     cineo.run(debug=True,port=3300)
