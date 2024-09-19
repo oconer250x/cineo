8bc381e86725ff440b8cb254a93da705e3af3eb9
oconer250x <mateo.bermejo5809@alumnos.udg.mx>
2024-09-18 18:02:38 -0600

    p10 models

---
+++ models/entities/User.py
@@ -0,0 +1,18 @@
+from werkzeug.security import check_password_hash
+from flask_login import UserMixin
+
+class User(UserMixin):
+    def __init__(self, id, nombre, correo, clave, fechareg, perfil) -> None:
+        self.id        = id
+        self.nombre    = nombre
+        self.correo    = correo
+        self.clave     = clave
+        self.fechareg  = fechareg
+        self.perfil    = perfil
+
+
+    @classmethod
+    def validarClave(self,claveCifrada,clave):
+        return check_password_hash(claveCifrada,clave)
+    
+    
