08615eaac9c7d31a66724c5dc7115e81c3b67b31
Kataprom <alan.hernandez1644@alumnos.udg.mx>
2024-10-23 18:08:32 -0600

    p19tablas

---
+++ static/SQL/cineo.sql
@@ -0,0 +1,222 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Servidor: localhost
+-- Tiempo de generación: 24-10-2024 a las 00:02:01
+-- Versión del servidor: 5.7.44-log
+-- Versión de PHP: 7.4.9
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Base de datos: `cineo`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `pelicula`
+--
+
+CREATE TABLE `pelicula` (
+  `idpelicula` int(11) NOT NULL,
+  `titulo` text NOT NULL,
+  `genero` text NOT NULL,
+  `fechadeestreno` date NOT NULL,
+  `sinopsis` text NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `perfiles`
+--
+
+CREATE TABLE `perfiles` (
+  `idperfiles` int(8) NOT NULL,
+  `biografia` text NOT NULL,
+  `avatar` varchar(255) NOT NULL,
+  `preferenciasdegenero` text NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `recomendaciones`
+--
+
+CREATE TABLE `recomendaciones` (
+  `idrecomendaciones` int(11) NOT NULL,
+  `usuarioindex` int(11) NOT NULL,
+  `peliculaindex` int(11) NOT NULL,
+  `score` float NOT NULL,
+  `fechaderecomendacion` timestamp(6) NOT NULL DEFAULT CURRENT_TIMESTAMP(6) ON UPDATE CURRENT_TIMESTAMP(6)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `reseñas`
+--
+
+CREATE TABLE `reseñas` (
+  `idreseñas` int(11) NOT NULL,
+  `usuarioindex` int(11) NOT NULL,
+  `peliculaindex` int(11) NOT NULL,
+  `calificacion` int(11) NOT NULL,
+  `comentario` text NOT NULL,
+  `fechadereseña` timestamp(6) NOT NULL DEFAULT CURRENT_TIMESTAMP(6) ON UPDATE CURRENT_TIMESTAMP(6)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `usuario`
+--
+
+CREATE TABLE `usuario` (
+  `id` int(11) NOT NULL,
+  `nombre` varchar(50) COLLATE utf8mb4_spanish_ci NOT NULL,
+  `correo` varchar(50) COLLATE utf8mb4_spanish_ci NOT NULL,
+  `clave` varchar(200) COLLATE utf8mb4_spanish_ci NOT NULL,
+  `fechareg` datetime NOT NULL,
+  `perfil` char(1) COLLATE utf8mb4_spanish_ci NOT NULL DEFAULT 'U'
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_spanish_ci;
+
+--
+-- Volcado de datos para la tabla `usuario`
+--
+
+INSERT INTO `usuario` (`id`, `nombre`, `correo`, `clave`, `fechareg`, `perfil`) VALUES
+(4, 'emiliano', 'kelsey.delarosa1743@alumnos.udg.mx', 'scrypt:32768:8:1$42IV98e7ZFO5eAfJ$7cfb94e046e412084a9627ffaf9016158d3701e542da6d53ae379d766375521eb3607ebd9dc8847de5a0d89eaa3f6208b89bb89eb6abc713679591060a29cdce', '2024-09-25 18:15:01', 'U'),
+(7, 'mateo', 'alan.hernandez1644@alumnos.udg.mx', 'scrypt:32768:8:1$0ZA9n6aJZJk3cB3v$7fc36a4e597a74244eb09edc5add933a827fb45f18b1daf4be0baf77c6c599c8db2869c6c60e1a5158336bf0058452d45a13a3aa096b1fe69370a18aa8cc1a89', '2024-09-25 18:22:27', 'U');
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `usuarionueva`
+--
+
+CREATE TABLE `usuarionueva` (
+  `idusuario` int(10) NOT NULL,
+  `nombre` text NOT NULL,
+  `email` text NOT NULL,
+  `fechaderegistro` timestamp(6) NOT NULL DEFAULT CURRENT_TIMESTAMP(6) ON UPDATE CURRENT_TIMESTAMP(6)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `usuarionuevo`
+--
+
+CREATE TABLE `usuarionuevo` (
+  `idusuario` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Índices para tablas volcadas
+--
+
+--
+-- Indices de la tabla `pelicula`
+--
+ALTER TABLE `pelicula`
+  ADD PRIMARY KEY (`idpelicula`);
+
+--
+-- Indices de la tabla `perfiles`
+--
+ALTER TABLE `perfiles`
+  ADD PRIMARY KEY (`idperfiles`);
+
+--
+-- Indices de la tabla `recomendaciones`
+--
+ALTER TABLE `recomendaciones`
+  ADD PRIMARY KEY (`idrecomendaciones`);
+
+--
+-- Indices de la tabla `reseñas`
+--
+ALTER TABLE `reseñas`
+  ADD PRIMARY KEY (`idreseñas`);
+
+--
+-- Indices de la tabla `usuario`
+--
+ALTER TABLE `usuario`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `correo` (`correo`);
+
+--
+-- Indices de la tabla `usuarionueva`
+--
+ALTER TABLE `usuarionueva`
+  ADD PRIMARY KEY (`idusuario`);
+
+--
+-- Indices de la tabla `usuarionuevo`
+--
+ALTER TABLE `usuarionuevo`
+  ADD PRIMARY KEY (`idusuario`);
+
+--
+-- AUTO_INCREMENT de las tablas volcadas
+--
+
+--
+-- AUTO_INCREMENT de la tabla `pelicula`
+--
+ALTER TABLE `pelicula`
+  MODIFY `idpelicula` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de la tabla `perfiles`
+--
+ALTER TABLE `perfiles`
+  MODIFY `idperfiles` int(8) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de la tabla `recomendaciones`
+--
+ALTER TABLE `recomendaciones`
+  MODIFY `idrecomendaciones` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de la tabla `reseñas`
+--
+ALTER TABLE `reseñas`
+  MODIFY `idreseñas` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de la tabla `usuario`
+--
+ALTER TABLE `usuario`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
+
+--
+-- AUTO_INCREMENT de la tabla `usuarionueva`
+--
+ALTER TABLE `usuarionueva`
+  MODIFY `idusuario` int(10) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de la tabla `usuarionuevo`
+--
+ALTER TABLE `usuarionuevo`
+  MODIFY `idusuario` int(11) NOT NULL AUTO_INCREMENT;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
